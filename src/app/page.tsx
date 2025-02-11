fd18cbaa3bc8499e663a2f9890e40400b789140f
Wallinson Rocha <wallinsonrochadev@gmail.com>
2025-02-11 13:28:49 -0300

    commit inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,146 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { useState, useEffect } from "react";
+import { Search } from 'lucide-react';
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+interface Song {
+    name: string;
+    category?: string;
+    musicLink?: string;
+    lyricsLink?: string;
+}
+
+export default function SongSearch() {
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [search, setSearch] = useState("");
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const apiKey = process.env.NEXT_PUBLIC_APIIDGOOGLE;
+    const spreadsheetId = process.env.NEXT_PUBLIC_SPREADSHEETID
+
+    // Url completa para a requisição
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Louvores!A:D?key= ${apiKey}`;
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                    console.error("Erro na requisição. Verifique mais tarde ou entre.")
+                }
+                const data = await response.json();
+                // Organização das 4 colunas
+                if (data.values) {
+                    const formattedSongs = data.values.slice(1).map((row: string[]) => ({
+                        name: row[0] || "",
+                        category: row[1] || "",
+                        musicLink: row[2] || "",
+                        lyricsLink: row[3] || "",
+                    }));
+                    setSongs(formattedSongs);
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.error("Algum erro no servidor: ", error)
+            }
+        }
+        fetchData();
+    }, []);
+
+    // Filtragem e organização das músicas
+    const filteredSongs = songs
+        .filter(song =>
+            song.name.toLowerCase().includes(search.toLowerCase()) ||
+            song.category?.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => a.name.localeCompare(b.name));
+
+    // Tela de Loading
+    if (loading) {
+        return (
+            <div className="bg-white h-screen content-center">
+                <p className="text-center text-gray-600">Carregando músicas...</p>
+            </div>
+        );
+    }
+
+    // Caso haja algum erro
+    if (!songs) {
+        return (
+            <div className="bg-white h-screen content-center">
+                <p className="text-center text-gray-600">Erro ao carregar as músicas. Tente mais tarde ou entre em contato com o suporte.</p>;
+            </div>
+        );
+    }
+
+    return (
+        <div className="p-6 bg-white text-black min-h-screen">
+            <div className="p-6 bg-white text-black">
+                {/* Início */}
+                <h1 className="text-6xl font-bold text-center">Louvores IPBetsaida</h1>
+                {/* Search bar */}
+                <div className="flex gap-2 items-center relative">
+                    <Search color="#00A63E" className="absolute left-2" size={32} />
+                    <input
+                        type="text"
+                        placeholder="Buscar música ou categoria..."
+                        className="w-full p-2 pl-12 border-2 border-green-600 rounded-2xl mb-4 mt-4"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
+            </div>
+            {/* Tabela de louvores */}
+            <div className="overflow-x-auto relative">
+                <table className="w-full text-sm">
+                    <thead className="bg-green-600 text-white sticky top-0 z-10">
+                        <tr className="text-left">
+                            <th className="p-3">Nome</th>
+                            <th className="p-3">Categoria</th>
+                            <th className="p-3">Música</th>
+                            <th className="p-3">Letra</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredSongs.map((song, index) => (
+                            <tr
+                                key={index}
+                                className={`${index % 2 === 0 ? 'bg-green-100' : 'bg-white'}`}
+                            >
+                                {/* Nome */}
+                                <td className="p-3">{song.name}</td>
+                                {/* Categoria */}
+                                <td className="p-3 flex flex-wrap gap-2">
+                                    {song.category ? (
+                                        song.category?.split(',').map((category, idx) => (
+                                            <span key={idx} className="bg-green-200 text-black px-3 py-1 rounded-full text-xs">
+                                                {category.trim()}
+                                            </span>
+                                        ))
+                                    ) : "—"}
+                                </td>
+                                {/* Link das músicas */}
+                                <td className="p-3">
+                                    {song.musicLink ? (
+                                        <a href={song.musicLink} className="text-green-500 underline" target="_blank" rel="noopener noreferrer">
+                                            Ouvir
+                                        </a>
+                                    ) : "—"}
+                                </td>
+                                {/* Letra */}
+                                <td className="p-3">
+                                    {song.lyricsLink ? (
+                                        <a href={song.lyricsLink} className="text-green-500 underline" target="_blank" rel="noopener noreferrer">
+                                            Ver Letra
+                                        </a>
+                                    ) : "—"}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
